1043296c8e4b1453c38d45e112632b1d245b3ce2
abhishekp-0 <132842566+abhishekp-0@users.noreply.github.com>
2024-07-19 10:31:13 +0530

    Update GroupA_Program1.cpp

--- GroupA_Program1.cpp
+++ GroupA_Program1.cpp
@@ -7,6 +7,9 @@ class Complex {
         //Constructors
         Complex() : real(0.0), img(0.0) {}  //Default Constructor
         Complex(double r, double i) : real(r), img(i) {} //User-defined Constructor
+        Complex(const Complex& other) : real(other.real), img(other.img) {}  //Copy Constructor
+        Complex(double r) : real(r), img(0.0) {}  //Conversion Constructor
+
 
         // Overload + operator for addition
         Complex operator+(const Complex& other) const {
@@ -63,5 +66,12 @@ int main() {
     // Output exmaple
     cout << "You entered: " << c4 << endl;
 
+    // Copy Constructor Usage
+    Complex c5(c1);     
+    cout << c5 << endl;
+    
+    // Conversion Constructor Usage
+    Complex c6(3.7);
+    cout << c6 << endl;
     return 0;
 }
