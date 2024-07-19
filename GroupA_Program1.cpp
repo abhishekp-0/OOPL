a8a8bdbeb3611f807bc6f8cb05a6fa8bd0275099
abhishekp-0 <132842566+abhishekp-0@users.noreply.github.com>
2024-07-19 09:49:07 +0530

    Update GroupA_Program1.cpp

--- GroupA_Program1.cpp
+++ GroupA_Program1.cpp
@@ -6,36 +6,37 @@ class Complex {
     public: 
         //Constructors
         Complex() : real(0.0), img(0.0) {}  //Default Constructor
-        Complex(double r, double i) : real(r), imaginary(i) {} //User-defined Constructor
+        Complex(double r, double i) : real(r), img(i) {} //User-defined Constructor
 
         // Overload + operator for addition
         Complex operator+(const Complex& other) const {
-            return Complex(real + other.real, imaginary + other.imaginary);
+            return Complex(real + other.real, img + other.img);
         }
 
         // Overload * operator for multiplication
         Complex operator*(const Complex& other) const {
-            double r = real * other.real - imaginary * other.imaginary;
-            double i = real * other.imaginary + imaginary * other.real;
+            double r = real * other.real - img * other.img;
+            double i = real * other.img + img * other.real;
             return Complex(r, i);
         }
         // Input and output operators
-        friend ostream& operator<<(ostream& os, const Complex& complex) {
-            os << complex.real << "+" << complex.imag << "i";
-            return os;
+        friend ostream& operator<<(ostream& output, const Complex& complex) {
+            output << complex.real << "+" << complex.img << "i";
+            return output;
         }
 
-        friend istream& operator>>(istream& is, Complex& complex) {
+        friend istream& operator>>(istream& input, Complex& complex) {
             cout << "Enter Real Part: ";
-            is >> complex.real;
+            input >> complex.real;
             cout << "Enter Imaginary Part: ";
-            is >> complex.imag;
-            return is;
+            input >> complex.img;
+            return input;
         }
 
     private:
-        double real;
-        double img;
+        // Data Members
+        double real;  //Real part
+        double img;   //Imaginary part
 };
 
 
@@ -58,6 +59,8 @@ int main() {
     // Input example
     Complex c4;
     cin >> c4;
+
+    // Output exmaple
     cout << "You entered: " << c4 << endl;
 
     return 0;
